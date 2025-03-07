c1e3f101af1ccdda330654dbc84eda4a47d322b1
Akm Korishee Apurbo <155003446+IMApurbo@users.noreply.github.com>
2025-03-08 00:34:47 +0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="LibScan",
+    name="libscan",
     version="1.0.0",
     author="AKM Korishee Apurbo",
     author_email="bandinvisible8@gmail.com",
-    description="LibScan: A Python dependency scanner and installer",
+    description="libscan: A Python dependency scanner and installer",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/IMApurbo/LibScan",  # Update with your repo
+    url="https://github.com/IMApurbo/libscan",  # Update with your repo
     packages=find_packages(),
     install_requires=[
         "rich",
